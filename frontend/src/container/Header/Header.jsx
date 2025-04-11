71731dcb9b1a7bcd3cf51981ad5d31b7d7a3e7b9
Yousif Hazim <105172397+YousifHazim@users.noreply.github.com>
2025-04-11 18:43:39 +0200

    Add files via upload

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 import { SubHeading } from "../../components";
-import images from "../../constants/images";
+import { images } from "../../constants";
 import "./Header.css";
 
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
-      <SubHeading title="chase the new flavour" />
-      <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1">The Key To Fine Dining</h1>
       <p className="p__opensans" style={{ margin: "2rem 0" }}>
         Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat
         morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet
@@ -18,8 +18,9 @@ const Header = () => (
         Explore Menu
       </button>
     </div>
+
     <div className="app__wrapper_img">
-      <img src={images.welcome} alt="header img" />
+      <img src={images.welcome} alt="header_img" />
     </div>
   </div>
 );
